acf1891abcd09d7b9af2a6a69d7caced4dcb61f4
Purple Team <118991961+purpleteamNSC@users.noreply.github.com>
2024-03-12 16:49:45 +0000

    doco

--- node/index.js
+++ node/index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people (name) values ('Diogo')`
+const del = `DELETE FROM people WHERE id=2`
+connection.query(del)
+connection.end()
+
 app.get('/',(req,res)=>{
-    res.send('<h1>Hello world</h1>')
+    res.send('<h1>Hello world 2</h1>')
 })
 
 app.listen(port, ()=>{
